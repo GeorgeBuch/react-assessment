dbc502db5ba69b005dc4d820a82f84aa7787f1f7
GeorgeBush <wps.wisit@gmail.com>
2026-08-28 18:07:53 +0700

    Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import SectionButtons from "../components/SectionButtons";
+
+function Home() {
+  return (
+    <main className="container">
+      <h1>
+        Generation Thailand
+        <br />
+        React - Assessment
+      </h1>
+
+      <SectionButtons />
+    </main>
+  );
+}
+
+export default Home;
