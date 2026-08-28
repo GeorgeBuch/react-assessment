6798e842f05e76e88e82400843590c9b6daa2b83
GeorgeBush <wps.wisit@gmail.com>
2026-08-28 18:08:44 +0700

    OwnerPage

---
+++ src/pages/OwnerPage.jsx
@@ -0,0 +1,19 @@
+function OwnerPage() {
+  return (
+    <main className="owner-page">
+      <h1>99 Your Name - JSDX</h1>
+
+      <div className="profile-picture">
+        picture
+      </div>
+
+      <h4>Short Biography:</h4>
+
+      <p>
+        Write your short biography here.
+      </p>
+    </main>
+  );
+}
+
+export default OwnerPage;
